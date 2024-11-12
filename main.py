ee30cb2b57a5034bde7f2dd0286808892136c1ef
unknown <natnaelm552@gmail.com>
2024-11-12 18:11:26 +0300

    body content extraction with bs4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scraper import scrape_website
+from scraper import scrape_website, extract_body_content, clean_body_content
 
 
 st.title("AI Web Scraper")
@@ -9,4 +9,11 @@ url = st.text_input("Enter the URL of the website you want to scrape:")
 if st.button("Scrape"):
   st.write("Scraping...")
   result = scrape_website(url)
-  print(result)
+  body_content = extract_body_content(result)
+  cleaned_content = clean_body_content(body_content)
+
+  st.session_state.dom_content = cleaned_content
+
+  with st.expander("Scraped Content"):
+    st.text_area('scraped content',cleaned_content , height=300 )
+     
--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import  selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
@@ -16,3 +17,21 @@ def scrape_website(url):
     return html
   finally:
     driver.quit()
+
+def extract_body_content(html):
+  soup = BeautifulSoup(html, 'html.parser')
+  body_content = soup.body
+  if body_content:
+    return str(body_content)
+  else:
+    return None
+  
+def clean_body_content(body_content):
+  soup = BeautifulSoup(body_content, 'html.parser')
+  for script_or_style in soup(["script", "style"]):
+    script_or_style.extract()
+
+  cleaned_content = soup.get_text(separator='\n')
+  cleaned_content = '\n'.join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+  return cleaned_content
